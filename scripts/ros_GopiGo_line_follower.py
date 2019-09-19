a35c865c33f9680a01b340a17be620d90cfa3c72
Matheus Nascimento <contamatheusnascimento@gmail.com>
2019-09-19 00:20:43 -0300

    Implement main structure

--- scripts/ros_GopiGo_line_follower.py
+++ scripts/ros_GopiGo_line_follower.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python
 
 import angles as ros_angles
-import tf
 from geometry_msgs.msg import Twist
-from gpg_remote.msg import State
+from turtlesim.msg import Pose
+
 import rospy
+# from gpg_remote.msg import State
+
+import time
 from nav_msgs.msg import Odometry
 
 from math import pow, atan2, sqrt
 
-# from __future__ import print_function
-# from __future__ import division
-# from builtins import input
-
-# the above lines are meant for Python3 compatibility.
-# they force the use of Python3 functionality for print(),
-# the integer division and input()
-# mind your parentheses!
-# import line_sensor
-# import time
-# import gopigo
-# import atexit
-
-
 class GoPiGoLineFollower:
     def __init__(self):
         rospy.init_node('GoPiGo_line_follower_node')
-        # self.initMemberVariables()
+        self.initMemberVariables()
         self.initROSChannels()
         self.followLine()
-    # def initMemberVariables(self):
-    #     self.current_pose = Odometry()
+
+    def initMemberVariables(self):
+        # Enables/Disables debug mode
+        self.debug = False
+        self.gpgEnable = False
+
+        # # Position to take action on
+        self.mid = [0, 0, 1, 0, 0]       # Middle Position.
+        self.mid1 = [0, 1, 1, 1, 0]      # Middle Position.
+        self.small_l = [0, 1, 1, 0, 0]   # Slightly to the left.
+        self.small_l1 = [0, 1, 0, 0, 0]  # Slightly to the left.
+        self.small_r = [0, 0, 1, 1, 0]   # Slightly to the right.
+        self.small_r1 = [0, 0, 0, 1, 0]  # Slightly to the right.
+        self.left = [1, 1, 0, 0, 0]      # Sensor reads strongly to the left.
+        self.left1 = [1, 0, 0, 0, 0]     # Sensor reads strongly to the left.
+        self.right = [0, 0, 0, 1, 1]     # Sensor reads strongly to the right.
+        self.right1 = [0, 0, 0, 0, 1]    # Sensor reads strongly to the right.
+        self.stop = [1, 1, 1, 1, 1]      # Sensor reads stop.
+        self.stop1 = [0, 0, 0, 0, 0]     # Sensor reads stop.
+
+        self.current_pose = Pose()
+
+        # [kl, ka, position(x,y,theta) + desired increments]
+        # todo: Update gains and increments
+        self.directions["goStraight"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
+        self.directions["turnSlightLeft"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
+        self.directions["turnLeft"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
+        self.directions["turnSlightRight"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
+        self.directions["stop"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
+        self.directions["goBack"] = [0.5, 0.5, (self.current_pose.x, self.current_pose.y, self.current_pose.theta + 0.01 )]
 
     def initROSChannels(self):
         self.pose_listener = rospy.Subscriber("gpg/state", State, self.callbackState)
@@ -38,7 +55,7 @@ class GoPiGoLineFollower:
 
     def callbackState(self, msg):
         self.line_sensors = msg.line
-        self.curr = self.lineInt2binary(self.line_sensors)
+        self.currentSensorSet = self.lineInt2binary(self.line_sensors)
 
     def lineInt2binary(self, line_int_values):
         output = []
@@ -49,153 +66,126 @@ class GoPiGoLineFollower:
                 output.append(1)
         return output
 
-    def speedController(self): #Continuar daqui!!!
+    def callbackPose(self, msg):
+        self.current_pose = msg
 
     def followLine(self):
+        rate = rospy.Rate(20)
+
+        while not rospy.is_shutdown():
+            # white line reached
+            if self.currentSensorSet == self.stop1 or self.currentSensorSet == self.stop:
+                if self.debug:
+                    rospy.loginfo("Out of line boundaries, stopping.")
+                    break
+            else:
+                self.moveGpg()
+            rate.sleep()
+
+    def moveGpg(self):
+        # if the line is in the middle, keep moving straight
+        # if the line is slightly left of right, keep moving straight
+        if (self.currentSensorSet == self.small_r or self.currentSensorSet == self.small_l or \
+                self.currentSensorSet == self.mid or self.currentSensorSet == self.mid1):
+            self.go_straight()
+
+        # If the line is towards the sligh left, turn slight right
+        elif self.currentSensorSet == self.small_l1:
+            self.turn_slight_right()
+        elif self.currentSensorSet == self.left or self.currentSensorSet == self.left1:
+            self.turn_right()
+
+        # If the line is towards the sligh right, turn slight left
+        elif self.currentSensorSet == self.small_r1:
+            self.turn_slight_left()
+        elif self.currentSensorSet == self.right or self.currentSensorSet == self.right1:
+            self.turn_left()
+        elif self.currentSensorSet == self.stop:
+            self.stop_now()
+
+        time.sleep(0.01)
+
+    def go_straight(self):
+        if self.debug:
+            print("Going straight")
+        if self.gpgEnable:
+            self.velocityController("goStraight")
+
+    def turn_slight_left(self):
+        if self.debug:
+            print("Turn slight left")
+        if self.gpgEnable:
+            self.velocityController("turnSlightLeft")
+
+    def turn_left(self):
+        if self.debug:
+            print("Turn left")
+        if self.gpgEnable:
+            self.velocityController("turnLeft")
+
+    def turn_slight_right(self):
+        if self.debug:
+            print("Turn slight right")
+        if self.gpgEnable:
+            self.velocityController("turnSlightRight")
+
+    def turn_right(self):
+        if self.debug:
+            print("Turn right")
+        if self.gpgEnable:
+            self.velocityController("turnRight")
+
+    def stop_now(self):
+        if self.debug:
+            print("Stop")
+        if self.gpgEnable:
+            self.velocityController("stop")
+
+    def go_back(self):
+        if self.debug:
+            print("Go Back")
+        if self.gpgEnable:
+            self.velocityController("goBack")
+
+    def velocityController(self, desired_direction):
+        vel_msg = Twist()
+
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+
+        distance_tolerance = 0.2
 
         rate = rospy.Rate(20)
 
-        while True:
-            print(self.curr)
+        linear_gain = self.directions[desired_direction][0]
+        angular_gain = self.directions[desired_direction][1]
+        goal_position = self.directions[desired_direction][2]
 
-            # white line reached
-            if self.curr == stop1:
-                if msg_en:
-                    print("White found, last cmd running")
-                for i in range(5):
-                    run_gpg(last_val)
-            else:
-                run_gpg(curr)
+        while self.euclideanDistance(goal_position) >= distance_tolerance:
 
+            vel_msg.linear.x = self.linearVelocity(goal_position, linear_gain)
+            vel_msg.angular.z = self.angularVelocity(goal_position, angular_gain)
+
+            self.velocity_publisher.publish(vel_msg)
             rate.sleep()
 
-    # atexit.register(gopigo.stop)  # When you ctrl-c out of the code, it stops the gopigo motors.
-    #
-    # # Calibrate speed at first run
-    # # 100 is good with fresh batteries
-    # # 125 for batteries with half capacity
-    #
-    # fwd_speed = 110  # Forward speed at which the GoPiGo should run.
-    # # If you're swinging too hard around your line
-    # # reduce this speed.
-    # poll_time = 0.01  # Time between polling the sensor, seconds.
-    #
-    # slight_turn_speed = int(.7 * fwd_speed)
-    # turn_speed = int(.7 * fwd_speed)
-    #
-    # last_val = [0] * 5  # An array to keep track of the previous values.
-    # curr = [0] * 5  # An array to keep track of the current values.
-    #
-    # gopigo.set_speed(fwd_speed)
-    #
-    gpg_en = 1  # Enable/disable gopigo
-    msg_en = 1  # Enable messages on screen.  Turn this off if you don't want messages.
-    #
-    # # Get line parameters
-    # line_pos = [0] * 5
-    # white_line = line_sensor.get_white_line()
-    # black_line = line_sensor.get_black_line()
-    # range_sensor = line_sensor.get_range()
-    # threshold = [a + b / 2 for a, b in
-    #              zip(white_line, range_sensor)]  # Make an iterator that aggregates elements from each of the iterables.
-    #
-    # # Position to take action on
-    mid = [0, 0, 1, 0, 0]  # Middle Position.
-    mid1 = [0, 1, 1, 1, 0]  # Middle Position.
-    small_l = [0, 1, 1, 0, 0]  # Slightly to the left.
-    small_l1 = [0, 1, 0, 0, 0]  # Slightly to the left.
-    small_r = [0, 0, 1, 1, 0]  # Slightly to the right.
-    small_r1 = [0, 0, 0, 1, 0]  # Slightly to the right.
-    left = [1, 1, 0, 0, 0]  # Sensor reads strongly to the left.
-    left1 = [1, 0, 0, 0, 0]  # Sensor reads strongly to the left.
-    right = [0, 0, 0, 1, 1]  # Sensor reads strongly to the right.
-    right1 = [0, 0, 0, 0, 1]  # Sensor reads strongly to the right.
-    stop = [1, 1, 1, 1, 1]  # Sensor reads stop.
-    stop1 = [0, 0, 0, 0, 0]  # Sensor reads stop.
-    #
-    # GoPiGo actions
-    def go_straight():
-        if msg_en:
-            print("Going straight")
-        if gpg_en:
-            gopigo.set_speed(fwd_speed)
-            gopigo.fwd()
+        # todo: Check if removing it is valid
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
 
-    def turn_slight_left():
-        if msg_en:
-            print("Turn slight left")
-        if gpg_en:
-            gopigo.set_right_speed(slight_turn_speed)
-            gopigo.set_left_speed(fwd_speed)
-            gopigo.fwd()
+        self.velocity_publisher.publish(vel_msg)
 
-    def turn_left():
-        if msg_en:
-            print("Turn left")
-        if gpg_en:
-            gopigo.set_speed(turn_speed)
-            gopigo.left()
+    def euclideanDistance(self, goal_pose):
+        return sqrt(pow((goal_pose.x - self.current_pose.x), 2) + pow((goal_pose.y - self.current_pose.y), 2))
 
-    def turn_slight_right():
-        if msg_en:
-            print("Turn slight right")
-        if gpg_en:
-            gopigo.set_right_speed(fwd_speed)
-            gopigo.set_left_speed(slight_turn_speed)
-            gopigo.fwd()
+    def linearVelocity(self, goal_pose, gain=1):
+        return gain * self.euclideanDistance(goal_pose)
 
-    def turn_right():
-        if msg_en:
-            print("Turn right")
-        if gpg_en:
-            gopigo.set_speed(turn_speed)
-            gopigo.right()
+    def angularVelocity(self, goal_pose, gain=4):
+        return gain * ros_angles.shortest_angular_distance(self.current_pose.theta, self.angleToTheGoal(goal_pose))
 
-    def stop_now():
-        if msg_en:
-            print("Stop")
-        if gpg_en:
-            gopigo.stop()
-
-    def go_back():
-        if msg_en:
-            print("Go Back")
-        if gpg_en:
-            gopigo.set_speed(turn_speed)
-            gopigo.bwd()
-
-    # Action to run when a line is detected
-    def run_gpg(curr):
-        # if the line is in the middle, keep moving straight
-        # if the line is slightly left of right, keep moving straight
-        if curr == small_r or curr == small_l or curr == mid or curr == mid1:
-            go_straight()
-
-        # If the line is towards the sligh left, turn slight right
-        elif curr == small_l1:
-            turn_slight_right()
-        elif curr == left or curr == left1:
-            turn_right()
-
-        # If the line is towards the sligh right, turn slight left
-        elif curr == small_r1:
-            turn_slight_left()
-        elif curr == right or curr == right1:
-            turn_left()
-        elif curr == stop:
-            stop_now()
-        time.sleep(poll_time)
-    #
-    # while True:
-    #     last_val = curr
-    #     curr = absolute_line_pos()
-    #     print(curr)
-    #
-    #     # white line reached
-    #     if curr == stop1:
-    #         if msg_en:
-    #             print("White found, last cmd running")
-    #         for i in range(5):
-    #             run_gpg(last_val)
-    #     else:
-    #         run_gpg(curr)
+    def angleToTheGoal(self, goal_pose):
+        return atan2(goal_pose.y - self.current_pose.y, goal_pose.x - self.current_pose.x)
