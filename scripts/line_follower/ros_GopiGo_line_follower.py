3a2d9a543121f33e0009fd4d6aa348f82f558122
Matheus Nascimento <contamatheusnascimento@gmail.com>
2019-09-21 19:27:26 -0300

    Update velocity controller

--- scripts/line_follower/ros_GopiGo_line_follower.py
+++ scripts/line_follower/ros_GopiGo_line_follower.py
@@ -42,10 +42,12 @@ import rospy
 import tf
 from gpg_remote.msg import State
 
-import time
 from nav_msgs.msg import Odometry
 
 from math import pow, atan2, sqrt
+
+import time
+import sys, select, termios, tty
 
 class GoPiGoLineFollower:
     def __init__(self):
@@ -73,24 +75,26 @@ class GoPiGoLineFollower:
         self.stop = [1, 1, 1, 1, 1]      # Sensor reads stop.
         self.stop1 = [0, 0, 0, 0, 0]     # Sensor reads stop.
 
+        # ################# Weird states ##################
+        self.weird_state_1 = [1, 0, 1, 1, 1]
+
         self.currentSensorSet = []
 
         self.current_pose = Odometry()
 
-        # [kl, ka, position(x,y,theta) + desired increments]
+        # [x,y,z,theta]
         # todo: Update gains and increments
         self.directions = {}
-        self.directions["goStraight"] = [1, 1, 0.1, 0.2, 0.1]
-        self.directions["turnSlightLeft"] = [1, 1, 0.1, 0.2, 0.6 ]
-        self.directions["turnLeft"] = [1, 1, 0.1, 0.2, 02]
-        self.directions["turnSlightRight"] = [1, 1, 0.01, 0.02, 0.6]
-        self.directions["turnRight"] = [1, 1, 0.1, 0.2, 0.6]
-        self.directions["stop"] = [1, 1, 0.1, 0.2, 0.6]
-        self.directions["goBack"] = [1, 1, 0.1, 0.2, 0.6]
+        self.directions["goStraight"] = [1, 0, 0, 0]
+        self.directions["turnSlightLeft"] = [1,0,0,1]
+        self.directions["turnLeft"] = [1, 0, 0, 1]
+        self.directions["turnSlightRight"] = [1,0,0,-1]
+        self.directions["turnRight"] = [0,0,0,-1]
+        self.directions["stop"] = [0, 0, 0, 0]
+        self.directions["goBack"] = [-1, 0, 0, 0]
 
     def initROSChannels(self):
         self.sensors_listener = rospy.Subscriber("gpg/state", State, self.callbackState)
-        self.pose_listener = rospy.Subscriber("gpg/mobile_base_controller/odom", Odometry, self.callbackPose)
         self.velocity_publisher = rospy.Publisher('gpg/mobile_base_controller/cmd_vel', Twist, queue_size=10)
 
 
@@ -108,25 +112,18 @@ class GoPiGoLineFollower:
                 output.append(1)
         return output
 
-    def callbackPose(self, msg):
-        self.current_pose = msg
-        self.curr_x = self.current_pose.pose.pose.position.x
-        self.curr_y = self.current_pose.pose.pose.position.y
-        self.curr_theta = self.yawFromQuaternion(self.current_pose.pose.pose.orientation)
-
-
     def followLine(self):
         rate = rospy.Rate(20)
 
         while not rospy.is_shutdown():
             # white line reached
-            print str(self.currentSensorSet)
-
-            if self.currentSensorSet == self.stop1 or self.currentSensorSet == self.stop:
+            if self.currentSensorSet == self.stop:
                 if self.debug:
-                    rospy.loginfo("Out of line boundaries, stopping.")
+                    rospy.loginfo("It has reached the Finish.")
             else:
-                if self.pose_listener.get_num_connections() > 0:
+                if self.sensors_listener.get_num_connections() > 0:
+                    if self.debug:
+                        print str(self.currentSensorSet)
                     time.sleep(0.01)
                     self.moveGpg()
             rate.sleep()
@@ -147,8 +144,8 @@ class GoPiGoLineFollower:
             self.turn_slight_left()
         elif self.currentSensorSet == self.right or self.currentSensorSet == self.right1:
             self.turn_left()
-        elif self.currentSensorSet == self.stop:
-            self.stop_now()
+        elif self.currentSensorSet == self.stop1:
+            self.go_back()
 
         time.sleep(0.01)
 
@@ -195,61 +192,21 @@ class GoPiGoLineFollower:
             self.velocityController("goBack")
 
     def velocityController(self, desired_direction):
+        speed = 0.05
+        turn = 0.2
+
+        y = 0
+        z = 0
+        x = 0
+        th = 0
+
+        x = self.directions[desired_direction][0]
+        y = self.directions[desired_direction][1]
+        z = self.directions[desired_direction][2]
+        th = self.directions[desired_direction][3]
+
         vel_msg = Twist()
-
-        vel_msg.linear.y = 0
-        vel_msg.linear.z = 0
-
-        vel_msg.angular.x = 0
-        vel_msg.angular.y = 0
-
-        distance_tolerance = 0.2
-
-        rate = rospy.Rate(20)
-
-        increment_x = self.directions[desired_direction][2]
-        increment_y = self.directions[desired_direction][3]
-        increment_theta = self.directions[desired_direction][4]
-
-        linear_gain = self.directions[desired_direction][0]
-        angular_gain = self.directions[desired_direction][1]
-
-
-        goal_position = (self.curr_x + increment_x, self.curr_y + increment_y, self.curr_theta + increment_theta)
-        # print "Angular gain = " + str(angular_gain)
-        print "Goal  = " + str(goal_position)
-
-        while self.euclideanDistance(goal_position) >= distance_tolerance:
-            vel_msg.linear.x = self.linearVelocity(goal_position, linear_gain)
-            vel_msg.angular.z = self.angularVelocity(goal_position, angular_gain)
-
-            self.velocity_publisher.publish(vel_msg)
-            rate.sleep()
-
-        # todo: Check if removing it is valid
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-
+        vel_msg.linear.x = x*speed; vel_msg.linear.y = y*speed; vel_msg.linear.z = z*speed;
+        vel_msg.angular.x = 0; vel_msg.angular.y = 0; vel_msg.angular.z = th * turn
 
         self.velocity_publisher.publish(vel_msg)
-
-    def euclideanDistance(self, goal_pose):
-        return sqrt(pow((goal_pose[0] - self.curr_x), 2) + pow((goal_pose[1] - self.curr_y), 2))
-
-    def linearVelocity(self, goal_pose, gain=1):
-        return gain * self.euclideanDistance(goal_pose)
-
-    def angularVelocity(self, goal_pose, gain=4):
-        return gain * ros_angles.shortest_angular_distance(self.curr_theta, self.angleToTheGoal(goal_pose))
-
-    def angleToTheGoal(self, goal_pose):
-        return atan2(goal_pose[1] - self.curr_y, goal_pose[0] - self.curr_x)
-
-    def yawFromQuaternion(self, quaternionMsg):
-        quaternionList = self.quaternionMsg2List(quaternionMsg)
-        euler = tf.transformations.euler_from_quaternion(quaternionList)
-        return euler[2]
-
-    def quaternionMsg2List(self, quaternionMsg):
-        quaternion = (quaternionMsg.x, quaternionMsg.y, quaternionMsg.z, quaternionMsg.w)
-        return quaternion
