ae89f4d1e99dd0725145cf629aa77d594e22b06b
Matheus Nascimento <contamatheusnascimento@gmail.com>
2019-11-25 13:53:53 -0600

    Update example using turtlesim node

--- scripts/robot_controller/ros_robot_controller.py
+++ scripts/robot_controller/ros_robot_controller.py
@@ -39,6 +39,7 @@ import angles as ros_angles
 import tf
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose2D
+import turtlesim.msg
 import rospy
 from nav_msgs.msg import Odometry
 
@@ -50,62 +51,70 @@ class RobotController:
         rospy.init_node('robot_pose_controller')
         self.initMemberVariables()
         self.initROSChannels()
-        rospy.spin()
+        self.move2goal()
 
     def initROSChannels(self):
         # The following lines are respective to exercise 2 item 'c'
-        self.pose_listener = rospy.Subscriber("gpg/mobile_base_controller/odom", Odometry, self.callbackPose)
+        # self.pose_listener = rospy.Subscriber("gpg/mobile_base_controller/odom", Odometry, self.callbackPose)
+        self.pose_listener = rospy.Subscriber("turtle1/pose", turtlesim.msg.Pose, self.callbackPose)
         self.goal_listener = rospy.Subscriber("gpg/goal", Pose2D, self.callbackGoal)
-        self.velocity_publisher = rospy.Publisher('gpg/mobile_base_controller/cmd_vel', Twist, queue_size=10)
+        # self.velocity_publisher = rospy.Publisher('gpg/mobile_base_controller/cmd_vel', Twist, queue_size=10)
+        self.velocity_publisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
 
     def initMemberVariables(self):
         self.current_pose = Odometry()
+        self.current_goal = Pose2D
 
     def callbackPose(self, msg):
         self.current_pose = msg
 
     def callbackGoal(self, msg):
-        current_goal = msg
-        self.goToGoal(current_goal)
+        self.current_goal = msg
 
     # The following lines are respective to exercise 2 item 'd'
-    def goToGoal(self, goal_position):
-        rospy.loginfo("Moving robot to the pose: (" + str(goal_position.x) + " , " + str(
-            goal_position.y) + ")" + " and theta = " + str(goal_position.theta))
+    def move2goal(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Waiting for the next goal...")
+            rospy.wait_for_message("gpg/goal", Pose2D)
+            goal_position = self.current_goal
+            rospy.loginfo("Moving robot to the pose: (" + str(goal_position.x) + " , " + str(
+                goal_position.y) + ")" + " and theta = " + str(goal_position.theta))
 
-        vel_msg = Twist()
+            vel_msg = Twist()
 
-        vel_msg.linear.y = 0
-        vel_msg.linear.z = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
 
-        vel_msg.angular.x = 0
-        vel_msg.angular.y = 0
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
 
-        distance_tolerance = 0.2
+            distance_tolerance = 0.2
 
-        rate = rospy.Rate(20)
+            rate = rospy.Rate(5)
 
-        while self.euclideanDistance(goal_position) >= distance_tolerance:
+            while self.euclideanDistance(goal_position) >= distance_tolerance:
+                vel_msg.linear.x = self.linearVelocity(goal_position)
+                vel_msg.angular.z = self.angularVelocity(goal_position)
 
-            vel_msg.linear.x = self.linearVelocity(goal_position)
-            vel_msg.angular.z = self.angularVelocity(goal_position)
+                self.velocity_publisher.publish(vel_msg)
+                rate.sleep()
+
+            vel_msg.linear.x = 0
+            vel_msg.angular.z = 0
 
             self.velocity_publisher.publish(vel_msg)
             rate.sleep()
 
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-
-        self.velocity_publisher.publish(vel_msg)
-
     def euclideanDistance(self, goal_pose):
-        return sqrt(pow((goal_pose.x - self.current_pose.pose.pose.position.x), 2) + pow((goal_pose.y - self.current_pose.pose.pose.position.y), 2))
+        # return sqrt(pow((goal_pose.x - self.current_pose.pose.pose.position.x), 2) + pow((goal_pose.y - self.current_pose.pose.pose.position.y), 2))
+        return sqrt(pow((goal_pose.x - self.current_pose.x), 2) + pow((goal_pose.y - self.current_pose.y), 2))
 
     def linearVelocity(self, goal_pose, gain=1):
         return gain * self.euclideanDistance(goal_pose)
 
     def angularVelocity(self, goal_pose, gain=4):
-        return gain * ros_angles.shortest_angular_distance(self.yawFromQuaternion(self.current_pose.pose.pose.orientation), self.angleToTheGoal(goal_pose))
+        # return gain * ros_angles.shortest_angular_distance(self.yawFromQuaternion(self.current_pose.pose.pose.orientation), self.angleToTheGoal(goal_pose))
+        return gain * ros_angles.shortest_angular_distance(self.current_pose.theta, self.angleToTheGoal(goal_pose))
 
     def yawFromQuaternion(self, quaternionMsg):
 
@@ -118,4 +127,4 @@ class RobotController:
         return quaternion
 
     def angleToTheGoal(self, goal_pose):
-        return atan2(goal_pose.y - self.current_pose.pose.pose.position.y, goal_pose.x - self.current_pose.pose.pose.position.x)
+        return atan2(goal_pose.y - self.current_pose.y, goal_pose.x - self.current_pose.x)
